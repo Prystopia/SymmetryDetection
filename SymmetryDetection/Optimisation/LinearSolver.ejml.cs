2db8eb0a74f70fd94a22ef4abf96f3a964070dc7
Edward Easton <edward.easton@prystopia.com>
2020-10-04 10:47:32 +0100

    added placeholder classes

---
+++ SymmetryDetection/Optimisation/LinearSolver.ejml.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SymmetryDetection.Optimisation
+{
+    public class LinearSolver
+    {
+        //https://github.com/lessthanoptimal/ejml/blob/e838418796934dd61e9f63ccb57e652653841088/main/ejml-ddense/src/org/ejml/dense/row/linsol/qr/LinearSolverQrHouseCol_DDRM.java#L48
+        public LinearSolver()
+        {
+        }
+    }
+}
---
+++ SymmetryDetection/Optimisation/QRDecomposition.ejml.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SymmetryDetection.Optimisation
+{
+    public class QRDecomposer
+    {
+        //https://github.com/lessthanoptimal/ejml/blob/e838418796934dd61e9f63ccb57e652653841088/main/ejml-ddense/src/org/ejml/dense/row/decomposition/qr/QRDecompositionHouseholderColumn_DDRM.java#L37
+        public QRDecomposition()
+        {
+        }
+    }
+}
